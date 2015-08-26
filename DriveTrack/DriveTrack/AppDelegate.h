6690ba5bf0eaf50b347753726a8469a449223df4
Eric Ludlow <esludlow@gmail.com>
2015-08-26 15:28:26 -0600

    InitialCommit

---
+++ DriveTrack/DriveTrack/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DriveTrack
+//
+//  Created by Rutan on 8/26/15.
+//  Copyright (c) 2015 EricLudlowRadicalApplications. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
